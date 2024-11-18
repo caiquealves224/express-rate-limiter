0a575a2136d35c7685a316693136388914c40dda
caique <caiquealves224@gmail.com>
2024-11-17 23:58:12 -0300

    feat: salving ip with indentifier key in redis

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -1,6 +1,16 @@
+import redisClient from "../redis";
+
 export const rateLimiter = (rule) => {
-    return (req, res, next) => {
-        const ipAddress = request.ip
+    return async (req, res, next) => {
+        const ipAddress = request.ip;
+        const key = `rate-limit:${ipAddress}`;
+
+        const requets = await redisClient.incr(key);
+
+        if(requets === 1) {
+            await redisClient.expire(key, rule.rate_limit.time)
+        }
+
         next()
     }
 }
